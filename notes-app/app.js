aaa662af752876f8e9fab922f4c9ea3cae08a372
LegolasZidane <laraibaa6@gmail.com>
2025-08-04 02:16:51 +0530

    first challenge completed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,5 @@
 const fs = require('fs');
+//file where you want the data to go, the data.
 fs.writeFileSync('notes.txt', 'This was from a synchronous function!');
+
+fs.appendFileSync('notes.txt', ' First Challenge Completed, Alhamdulilah!!!!!');

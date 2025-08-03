c33a8f4f062fea238aaa6731c42126466a947323
LegolasZidane <laraibaa6@gmail.com>
2025-08-04 03:03:19 +0530

    lesson 10 done

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,25 @@
-const fs = require('fs');
-//file where you want the data to go, the data.
-fs.writeFileSync('notes.txt', 'This was from a synchronous function!');
+// const fs = require('fs');
+// //file where you want the data to go, the data.
+// fs.writeFileSync('notes.txt', 'This was from a synchronous function!');
 
-fs.appendFileSync('notes.txt', ' First Challenge Completed, Alhamdulilah!!!!!');
+// fs.appendFileSync('notes.txt', ' First Challenge Completed, Alhamdulilah!!!!!');
+
+// require('./utils.js');
+
+// const name = 'Laraib';
+
+// console.log(name);
+
+// const name = require('./utils.js');
+
+// console.log(name);
+
+// const add = require('./utils.js');
+
+// const sum = add(4, -2);
+
+// console.log(sum);
+
+const getNotes = require('./notes.js');
+
+console.log(getNotes());
---
+++ notes-app/utils.js
@@ -0,0 +1,12 @@
+// console.log('utils.js');
+
+// const name = 'Laraib';
+
+// module.exports = name;
+
+// const add = function (a, b) {
+//     return a + b;
+// }
+
+// module.exports = add
+
